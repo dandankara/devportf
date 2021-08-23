c7df4c6e831aa31962ca57eaae737a3b9a3315a0
dandankara <cunhacb22@gmail.com>
2021-08-23 17:48:47 -0300

    Add Page Stacks

--- src/pages/stacks.js
+++ src/pages/stacks.js
@@ -1,14 +1,51 @@
-import styles from '../styles/Stacks.module.css'
+import Html from '../../public/Icons/html5.svg'
+import Css from '../../public/Icons/css3.svg'
+import Javascript from '../../public/Icons/javascript.svg'
+import React from '../../public/Icons/react.svg'
+import NextJs from '../../public/Icons/nextdotjs.svg'
+import Styled from '../../public/Icons/styledcomponents.svg'
 
-export default function Stacks(){
-  return(
+import styles from '../styles/Stacks.module.css';
+import Image from 'next/image';
+
+export default function Stacks() {
+  return (
     <div className={styles.Container}>
-      <div className={styles.items}><h1>Html</h1> </div>
-      <div className={styles.items}><h1>JavasScript</h1> </div>
-      <div className={styles.items}><h1>Css</h1> </div>
-      <div className={styles.items}><h1>React</h1> </div>
-      <div className={styles.items}><h1>NextJs</h1> </div>
-      <div className={styles.items}><h1>Styled Components</h1> </div>
+
+      <span>
+        <h1>Html</h1>
+        <Image src={Html} alt="html" />
+      </span>
+
+      <span>
+      <h1>Css</h1>
+      <Image src={Css} alt="css" />
+      </span>
+
+
+      <span>
+      <h1>Javascript</h1>
+      <Image src={Javascript} alt="javascript" />
+      </span>
+
+
+      <span>
+      <h1>React</h1>
+      <Image src={React} alt="react" />
+      </span>
+
+
+      <span>
+      <h1>NextJs</h1>
+      <Image src={NextJs} alt="next" />
+      </span>
+
+
+      <span>
+      <h1>Styled Components</h1>
+      <Image src={Styled} alt="styled" />
+      </span>
+
     </div>
   )
 }
